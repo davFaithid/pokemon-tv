7dc0eb54f8e7bc3ca5fa3d7200c5eb0f03054c76
davFaithid <42425325+davFaithid@users.noreply.github.com>
2019-12-31 18:05:20 -0500

    0.0.2

--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
 from setuptools import setup
+import py2exe
+
+with open("README.markdown", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='pokemon-tv',
-    version='0.0.1',
-    packages=['src', 'src.ytdl', 'src.ytdl.extractor', 'src.ytdl.downloader', 'src.ytdl.extractortest',
-              'src.ytdl.postprocessor'],
+    version='0.0.2',
+    packages=['pokemon-tv', 'pokemon-tv.ytdl', 'pokemon-tv.ytdl.extractor', 'pokemon-tv.ytdl.downloader', 'pokemon-tv.ytdl.extractortest',
+              'pokemon-tv.ytdl.postprocessor'],
     url='https://github.com/davFaithid/pokemon-tv',
     license='',
     author='davFaithid',
     author_email='',
-    description='Watch (currently only the first season) of Pokemon in python3.'
+    description='Watch (currently only the first season) of Pokemon in python3.',
+    console=['pokemon-tv/__init__.py'],
+    options={
+            "py2exe":{
+                    "unbuffered": True,
+                    "optimize": 2,
+                    "excludes": ["email"]
+                }
+        }
 )
--- src/ytdl/extractortest/extractors.py
+++
@@ -1,7 +0,0 @@
-# flake8: noqa
-from __future__ import unicode_literals
-
-from .pokemon import (
-    PokemonIE,
-    PokemonWatchIE,
-)
